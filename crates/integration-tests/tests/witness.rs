5ca116957139593e344c4db28d1bdadedc32b7ba
Ignacio Hagopian <jsign.uy@gmail.com>
2026-01-08 17:16:36 -0300

    include openvm trie

--- crates/integration-tests/tests/witness.rs
+++ crates/integration-tests/tests/witness.rs
@@ -6,6 +6,7 @@ use guest::{Guest, Platform};
 use integration_tests::{
     fixtures_dir, stateless_validator::StatelessValidatorFixture, untar_fixtures,
 };
+use openvm_mpt::statelesstrie::OpenVMStatelessSparseTrie;
 use sparsestate::SparseState;
 // use openvm_mpt::statelesstrie::OpenVMStatelessSparseTrie;
 use stateless_validator_reth::guest::{
@@ -32,18 +33,50 @@ async fn sparse_mpts() {
         }
         println!("Processing fixture: {}", fixture.name);
         let input = StatelessValidatorRethInput::new(&fixture.stateless_input).unwrap();
+
         // Reth with Risc0 sparse MPT.
         StatelessValidatorRethGuestWithTrie::<SparseState>::compute::<NoopPlatform>(input.clone());
 
-        // // Reth with OpenVM sparse MPT.
-        // {
-        //     let input = transform_witness(input);
+        // Reth with OpenVM sparse MPT.
+        {
+            let input = transform_witness(input);
 
-        //     RethStatelessValidatorGuest::<OpenVMStatelessSparseTrie>::compute::<NoopPlatform>(
-        //         input,
-        //     );
-        // }
+            StatelessValidatorRethGuestWithTrie::<OpenVMStatelessSparseTrie>::compute::<NoopPlatform>(
+                input.clone(),
+            );
+        }
     }
+}
+
+/// Transforms the witness in the input from Risc0 MPT format to OpenVM MPT format.
+pub fn transform_witness(mut input: StatelessValidatorRethInput) -> StatelessValidatorRethInput {
+    let pre_state_root = state_root_from_headers(
+        input.stateless_input.block.number - 1,
+        &input.stateless_input.witness.headers,
+    );
+    let tries_bytes = openvm_mpt::from_proof::from_execution_witness(
+        pre_state_root,
+        &input.stateless_input.witness,
+    )
+    .unwrap()
+    .encode_to_state_bytes();
+
+    // This is just a hacky way to pass the EthereumStateBytes to the guest program
+    // without changing the interface. The guest program will simply take the first
+    // item of the `witness.state` vector and decode it as EthereumStateBytes.
+    let bytes = bincode::serialize(&tries_bytes).unwrap();
+    input.stateless_input.witness.state = vec![bytes.into()];
+    input
+}
+
+fn state_root_from_headers(block_num: u64, headers: &[impl AsRef<[u8]>]) -> alloy_primitives::B256 {
+    headers
+        .iter()
+        .find_map(|h| {
+            let header = alloy_rlp::decode_exact::<alloy_consensus::Header>(h).unwrap();
+            (header.number == block_num).then_some(header.state_root)
+        })
+        .unwrap()
 }
 
 pub(crate) struct NoopPlatform;
